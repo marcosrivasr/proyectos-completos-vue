2405319fd4aa7d390c5c6cadcc9c5536a68e3316
Marcos Rivas <marcosrivasr@gmail.com>
2022-07-14 00:47:30 -0500

    app 05

---
+++ 05-menu/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Menu from "./components/Menu.vue";
+</script>
+
+<template>
+  <Menu />
+</template>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ 05-menu/src/components/MenuItem.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps(["item", "collapsed"]);
+</script>
+
+<template>
+  <a href="#" @click.prevent="props.item.onClick">
+    <div class="item">
+      <div class="icon">
+        <font-awesome-icon :icon="['fa-solid', props.item.icon]" size="2x" />
+      </div>
+      <div class="title" v-if="!collapsed">
+        {{ props.item.title }}
+      </div>
+    </div>
+  </a>
+</template>
+
+<style scoped>
+a {
+  color: white;
+  text-decoration: none;
+  display: block;
+}
+a:hover {
+  background-color: white;
+  color: black;
+}
+.item {
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  gap: 10px;
+}
+
+.item .icon {
+  width: 80px;
+  display: flex;
+  justify-content: center;
+}
+</style>
